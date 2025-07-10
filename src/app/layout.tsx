2515ffaa4db9789558e9d5593fd47534967900b7
JayceY443 <jayce.y.443@icloud.com>
2025-07-10 15:46:09 +0800

    feat: remove useless fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,53 +3,11 @@ import './globals.css'
 import localFont from 'next/font/local'
 import { ThemeProvider } from 'next-themes'
 
-const sourceHanSans = localFont({
-  src: [
-    {
-      path: '../../public/fonts/SourceHanSansSC-Bold.otf',
-      weight: '700',
-      style: 'normal'
-    },
-    {
-      path: '../../public/fonts/SourceHanSansSC-Regular.otf',
-      weight: '400',
-      style: 'normal'
-    },
-    {
-      path: '../../public/fonts/SourceHanSansSC-Light.otf',
-      weight: '300',
-      style: 'normal'
-    },
-  ],
-  variable: '--font-source-han-sans',
-  display: 'swap'
-})
-
 const SourceHanSerif = localFont({
   src: [
     {
-      path: '../../public/fonts/SourceHanSerifCN-ExtraLight.otf',
-      weight: '200',
-      style: 'normal'
-    },
-    {
       path: '../../public/fonts/SourceHanSerifCN-Medium.otf',
       weight: '400',
-      style: 'normal'
-    },
-    {
-      path: '../../public/fonts/SourceHanSerifCN-Medium.otf',
-      weight: '500',
-      style: 'normal'
-    },
-    {
-      path: '../../public/fonts/SourceHanSerifCN-Heavy.otf',
-      weight: '800',
-      style: 'normal'
-    },
-    {
-      path: '../../public/fonts/SourceHanSerifCN-SemiBold.otf',
-      weight: '600',
       style: 'normal'
     },
     {
@@ -82,18 +40,12 @@ export default function RootLayout({
         />
         <link 
           rel="preload" 
-          as="font" 
-          type="font/woff2" 
-          href="https://rbrzxzkxabhaeiljimvy.supabase.co/storage/v1/object/public/portfolio//SourceHanSansSC-Light.woff2" 
-        />
-        <link 
-          rel="preload" 
           as="image" 
           href="https://rbrzxzkxabhaeiljimvy.supabase.co/storage/v1/object/public/portfolio//20250710-134107.jpeg" 
         />
       </head>
       <body
-        className={`${sourceHanSans.variable} ${SourceHanSerif.variable} antialiased`}
+        className={`${SourceHanSerif.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
