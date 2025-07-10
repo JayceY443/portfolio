44bff9cfd85afe31cf0edf86fbc0cf11fef1272b
JayceY443 <jayce.y.443@icloud.com>
2025-07-10 16:40:49 +0800

    feat: change language of this website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="zh-CN" suppressHydrationWarning>
       <head>
         <link 
           rel="preload" 
